9b5782134899dc09e7a14d52ae9fdb1c04994d86
Virginia Davenport <virginialdport@gmail.com>
2020-07-07 16:22:08 -0600

    finished through GET request to /api/posts/:id

--- posts/posts-router.js
+++ posts/posts-router.js
@@ -22,6 +22,26 @@ router.post("/", (req, res) => {
 })
 
 //client makes a POST request to /api/posts/:id/comments
+router.post("/:id/comments", (req, res) => {
+    const newComment = req.body;
+
+    Posts.findById(req.params.id)
+        .then(post => {
+            if(!post){
+                res.status(404).json({message: "The post with the specified ID does not exist."})
+            } else if (!newComment.text){
+                res.status(400).json({errorMessage: "Please provide text for the comment"})
+            } else {
+                res.status(201).json("Created")
+            }
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(500).json({error: "There was an error while saving the comment to the database"})
+        })
+    
+})
+
 //when the client makes a GET request to /api/posts
 router.get("/", (req, res) => {
     Posts.find(req.query)
@@ -36,7 +56,26 @@ router.get("/", (req, res) => {
 })
 
 //when the client makes a GET request to /api/posts/id
+router.get("/:id", (req, res) => {
+    Posts.findById(req.params.id)
+        .then(post => {
+            if(post){
+                res.status(200).json(post)
+            } else {
+                res.status(404).json({message:"The post with the specidied ID does not exist"})
+            }
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(500).json({error: "The post information could not be retrieved."})
+        })
+})
+
+
 //when the client makes a GET request to /api/posts/:id/comments
+router.get("/:id/comments", (req, res) => {
+    
+})
 //when the client makes a DELETE request to /api/posts/:id
 //when the client makes a PUT request to /api/posts/:id
 module.exports = router
